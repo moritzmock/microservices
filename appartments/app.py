7d8804f3da0ca74f1369ec967823f8ba148f2bfd
moritzmock <mock.moritz@rolmail.net>
2021-12-14 17:09:51 +0100

    worked on the removing of the appartment

--- appartments/app.py
+++ appartments/app.py
@@ -17,14 +17,17 @@ app = Flask(__name__)
 def add():
     id = uuid.uuid4()
     name = request.args.get("name")
+    size = request.args.get("size")
 
     if name == None:
         return Response('{"result": false, "error": 1, "description": "Cannot proceed because you did not provide a name for the appartment."}', status=400, mimetype="application/json")
+    if size == None:
+            return Response('{"result": false, "error": 3, "description": "Cannot proceed because you did not provide a size for the appartment."}', status=400, mimetype="application/json")
 
     # Connect and setup the database
     connection = sqlite3.connect("/home/data/appartments.db", isolation_level=None)
     cursor = connection.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS appartments (id text, name text)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS appartments (id text, name text, size text)")
 
     # Check if appartement already exists
     cursor.execute("SELECT COUNT(id) FROM appartments WHERE name = ?", (name,))
@@ -33,7 +36,7 @@ def add():
         return Response('{"result": false, "error": 2, "description": "Cannot proceed because this appartment already exists"}', status=400, mimetype="application/json")
 
     # Add appartement
-    cursor.execute("INSERT INTO appartments VALUES (?, ?)", (str(id), name))
+    cursor.execute("INSERT INTO appartments VALUES (?, ?, ?)", (str(id), name, size))
     cursor.close()
     connection.close()
 
@@ -46,6 +49,41 @@ def add():
 
     return Response('{"result": true, description="Appartment was added successfully."}', status=201, mimetype="application/json")
 
+@app.route("/remove")
+def remove():
+
+    name = request.args.get("name")
+
+    if name == None:
+        return Response('{"result": false, "error": 1, "description": "Cannot proceed because you did not provide a name for the appartment."}',status=400, mimetype="application/json")
+
+    # Connect and setup the database
+    connection = sqlite3.connect("/home/data/appartments.db", isolation_level=None)
+    cursor = connection.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS appartments (id text, name text, size text)")
+
+    # Check if appartement exists
+    cursor.execute("SELECT COUNT(id) FROM appartments WHERE name = ?", (name,))
+    exists = cursor.fetchone()[0]
+
+    if exists == 0:
+        return Response(
+            '{"result": false, "error": 2, "description": "Can not proceed because this appartment does not exists"}',
+            status=400, mimetype="application/json")
+
+    # remove appartement
+    cursor.execute("DELETE FROM appartments WHERE name = (?)", (name,))
+    cursor.close()
+    connection.close()
+
+    # Notify everybody that the appartment was added
+    connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
+    channel = connection.channel()
+    channel.exchange_declare(exchange="appartments", exchange_type="direct")
+    channel.basic_publish(exchange="appartments", routing_key="removed", body=json.dumps({"name": name}))
+    connection.close()
+
+    return Response('{"result": true, description="Appartment was removed successfully."}', status=201, mimetype="application/json")
 
 @app.route("/")
 def hello():
